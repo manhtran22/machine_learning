1c4527f383e5f5c87cd0f21191308dc4eb3c6986
tranmanh <tb.tienmanh@mail.ru>
2024-04-17 13:01:08 +0300

    1

--- 7_TCP/client.py
+++ 7_TCP/client.py
@@ -24,11 +24,14 @@ path=os.path.join(os.path.abspath(os.curdir) , 'my_model.onnx')
 args_confidence = 0.2
 
 # initialize the list of class labels 
-CLASSES = ['KLUBNIKA', 'raspberry']
-
+CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
+	"bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
+	"dog", "horse", "motorbike", "person", "pottedplant", "sheep",
+	"sofa", "train", "tvmonitor"]
+COLORS = np.random.uniform(0, 255, size=(len(CLASSES), 3))
 # load our serialized model from disk
 print("[INFO] loading model...")
-net = cv2.dnn.readNetFromONNX (path)
+net = cv2.dnn.readNetFromCaffe(txt, caffemodel)
 ########################
 
 def video_stream():
@@ -50,7 +53,23 @@ def video_stream():
 		blob = cv2.dnn.blobFromImage(cv2.resize(frame, (32, 32)),scalefactor=1.0/32
 								, size=(32, 32), mean= (128,128,128), swapRB=True)
 		cv2.imshow("Cropped image", cv2.resize(frame, (32, 32)))
+		for i in np.arange(0, detections.shape[2]):
+			confidence = detections[0, 0, i, 2]
+        # filter out weak detections by ensuring the `confidence` is
+        # greater than the minimum confidence
+			if confidence > args_confidence:
+        # extract the index of the class label from the
+        # `detections`, then compute the (x, y)-coordinates of
+        # the bounding box for the object
+				idx = int(detections[0, 0, i, 1])
+				box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+				(startX, startY, endX, endY) = box.astype("int")
 
+            # draw the prediction on the frame
+				label = "{}: {:.2f}%".format(CLASSES[idx],confidence * 100)
+				cv2.rectangle(frame, (startX, startY), (endX, endY),COLORS[idx], 2)
+				y = startY - 15 if startY - 15 > 15 else startY + 15
+				cv2.putText(frame, label, (startX, y),cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)
 		# pass the blob through the network and obtain the detections and
 		# predictions
 		net.setInput(blob)
